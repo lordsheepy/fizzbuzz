956f5b478e1ed48de402949970b8a3dea2839f32
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-13 11:51:30 -0800

    Add user input from shell

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,19 @@ def fizzbuzz(n, divisor_dict):
         return str(n)
 
     return result
+
+
+if __name__ == "__main__":
+
+    num = int(raw_input("What number would you like to evaluate?"))
+    input_dict = {}
+    more = True
+
+    while more == True:
+        divisor = int(raw_input("Please input a divisor: "))
+        word = str(raw_input("What word you like to have it print? "))
+        input_dict[divisor] = word
+        y_or_n = str(raw_input("Would you like to add another divisor?(Y/N) "))
+        more = y_or_n.lower() == 'y'
+
+    print fizzbuzz(num, input_dict)
