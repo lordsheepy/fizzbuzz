3ddf5a0bdfe35804c8dc38cab78dadfb7e2634e5
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-13 11:17:56 -0800

    Fussing

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,15 @@
-
+#! usr/bin/env/python
 def fizzbuzz(n, divisor_dict):
 
     """Creating a user extensible fizzbuzz in which user provide a dict with a
     key corresponding to divisor and a value equating the desired string"""
+
     result = ''
     for k, v in sorted(divisor_dict.items()):
         if n % k == 0:
-            result.append(v)
+            result += v
+
     if result == '':
         return n
+
     return result
