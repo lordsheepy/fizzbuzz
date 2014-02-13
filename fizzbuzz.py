a5b8422a7f6c535546f4f548fd0d81f0cda37e81
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-13 10:03:52 -0800

    convert comment to docstring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
-"""Creating a user extensible fizzbuzz in which user provide a dict with a key
-corresponding to divisor and a value equating the desired string"""
-
 
 def fizzbuzz(n, divisor_dict):
+
+    """Creating a user extensible fizzbuzz in which user provide a dict with a
+    key corresponding to divisor and a value equating the desired string"""
     result = ''
     for k, v in sorted(divisor_dict.items()):
         if n % k == 0:
